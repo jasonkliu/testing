010954e8e29b7fe03ca6013c35d7f94948d53466
Jason Liu <jason@jasonkliu.com>
2014-07-01 22:00:11 -0700

    New db migration, update .rspec file

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623170109) do
+ActiveRecord::Schema.define(version: 20140628030918) do
 
   create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "widgets", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
